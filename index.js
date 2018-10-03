3ff536106fd604807a7788d4356a9b2cd5af4868
Matthew Augustyn <ma62@illinois.edu>
2018-10-02 21:29:28 -0500

    Added commands, bugfixes
    -Added a command listener that splits args
    -Ironed out some bugs with listening and the like

--- index.js
+++ index.js
@@ -3,15 +3,99 @@ const Discord = require("discord.js");
 // import the config file
 const config = require("./config.json");
 
-// load the bot token from the local config file
-const TOKEN = config.token;
-// create an instance of the Discord client
-var bot = new Discord.client();
+// create an instance of the Discord client and log in with the provided token in config.json
+var bot = new Discord.Client();
 
-// triggers whenever the bot detects a message
-bot.on("message", function(message) {
-	console.log(message.content);
+// load variables from the config
+const AUTH_LEVEL_NAME = config.authNames;
+// authorization levels 0=none, 1=basic priveleges, 2=trusted privileges, 3=admin
+const DEFAULT = 0;
+const INITIATED = 1;
+const TRUSTED = 2;
+const ADMIN = 3;
+// STUB load channels
+const lounge = bot.channels.find("name", "lounge")
+const testChannel = bot.channels.find("name", "bot-commands")
+
+// authorization fail message
+const AUTH_FAILED = '`hmmmm` :thonkBot: `It seems you do not have authorization to use that command`';
+// method to check authLevel
+function isAuth(user, levelRequired)
+{
+	switch (levelRequired)
+	{
+		case DEFAULT:
+			if (user.roles.includes(AUTH_LEVEL_NAME[DEFAULT]))
+			{
+				return true;
+			}
+		case INITIATED:
+			if (user.roles.includes(AUTH_LEVEL_NAME[INITIATED]))
+			{
+				return true;
+			}
+		case TRUSTED:
+			if (user.roles.includes(AUTH_LEVEL_NAME[TRUSTED]))
+			{
+				return true;
+			}
+		case ADMIN:
+			if (user.roles.includes(AUTH_LEVEL_NAME[ADMIN]))
+			{
+				return true;
+			}
+		default:
+			return false;
+	}
+}
+
+bot.on("ready", () =>
+{
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  bot.user.setActivity(`with its config files`);
 });
 
-// logs in with the declared authentification token
-bot.login(TOKEN);
+// command listener
+//bot.on('message', function(user, userID, channelID, message, evt)
+bot.on('message', function(message)
+{
+	// the key prefix for thonkBot is `?`
+    if (message.content.substring(0, 1) == config.prefix)
+	{
+		// STUB logs every recieved command in console
+		console.log(message.content);
+		// STUB respond with joke for testing
+		message.channel.send("oi")
+			.then(message => console.log("Sent response: " + message.content))
+			.catch(console.error);
+		// get command name and passed args
+        var args = message.content.substring(1).split(' ');
+		console.log(args);
+        var cmd = args[0];
+        args = args.splice(1);
+		console.log(args);
+        switch(cmd)
+		{
+            // ?ping <recipient> <number of pings>
+            case 'ping':
+				for (i = 0; i < args[1]; i++)
+				{
+					message.channel.send(args[0]);
+				}
+            break;
+			default:
+			break;
+         }
+	}
+});
+
+bot.login(config.token);
+
+
+
+
+
+
